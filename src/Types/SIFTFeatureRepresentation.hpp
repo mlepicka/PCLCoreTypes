20c39fd0bd7cf3cb00244c5f41ba60752f563827
Tomasz Kornuta <tkornuta@gmail.com>
2015-06-01 12:08:37 +0200

    SIFTFeatureRepresentation

---
+++ src/Types/SIFTFeatureRepresentation.hpp
@@ -0,0 +1,35 @@
+#ifndef SIFTFeatureRepresentation_HPP_
+#define SIFTFeatureRepresentation_HPP_
+
+#define PCL_NO_PRECOMPILE
+#include <pcl/point_types.h>
+#include <Types/PointXYZSIFT.hpp>
+
+
+/*
+ * \brief Class used for transformation from SIFT descriptor to array of floats.
+ */
+class SIFTFeatureRepresentation: public pcl::DefaultFeatureRepresentation <PointXYZSIFT> //could possibly be pcl::PointRepresentation<...> ??
+{
+	/// Templatiated number of SIFT descriptor dimensions.
+	using pcl::PointRepresentation<PointXYZSIFT>::nr_dimensions_;
+
+	public:
+	SIFTFeatureRepresentation ()
+	{
+		// Define the number of dimensions.
+		nr_dimensions_ = 128 ;
+		trivial_ = false ;
+	}
+
+	/// Overrides the copyToFloatArray method to define our feature vector
+	virtual void copyToFloatArray (const PointXYZSIFT &p, float * out) const
+	{
+		//This representation is only for determining correspondences (not for use in Kd-tree for example - so use only the SIFT part of the point.
+		for (register int i = 0; i < 128 ; i++)
+			out[i] = p.descriptor[i];//p.descriptor.at<float>(0, i) ;
+	}
+};
+
+
+#endif /* SIFTFeatureRepresentation_HPP_ */
